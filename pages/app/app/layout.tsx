46de6e754e312559371ae7edd0ca3035f2d1a1c2
HutchinsT <hutchins.tm3661@gmail.com>
2026-02-15 23:59:12 +0800

    Delete pages/app directory

--- pages/app/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import './globals.css'
-export const metadata = { title: '我的護城河課程', description: '2026 實戰營' }
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="zh-TW">
-      <body>{children}</body>
-    </html>
-  )
-}
